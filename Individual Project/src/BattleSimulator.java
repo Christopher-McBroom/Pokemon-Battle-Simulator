8544238825e030d538db2d53d7e570a2ccc15895
christopher.mcbroom <christopher.mcbroom@MU-C1-17.mullen.pvt.k12.co.us>
2023-11-06 13:00:44 -0700

    Comment for testing

--- Individual Project/src/BattleSimulator.java
+++ Individual Project/src/BattleSimulator.java
@@ -8,6 +8,7 @@ public class BattleSimulator
 		public static void main(String[] args)
 			{
 				System.out.println("Hello and welcome to the Java Pokémon Battle Simulator! \nNow please select the Pokémon you wish to play with: Bulbasaur(1), Charmander(2), Squirtle(3), Random Pokémon(4) .");
+				//Super epic comment
 			}
 
 	}
